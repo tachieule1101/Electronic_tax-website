3e07f9d7ede1b4434ea75b3ba2ce552a23cd8f53
Your Name <you@example.com>
2020-07-20 15:31:45 +0700

    LayoutVietHoa

---
+++ VietHoaLayout/VietHoaLayout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VietHoaLayout.MvcApplication" Language="C#" %>
---
+++ VietHoaLayout/VietHoaLayout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VietHoaLayout.Startup))]
+namespace VietHoaLayout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
